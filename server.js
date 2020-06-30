bdf5130ae9825aa8ecb22f4e886f40a31465caba
Abdus8Samad <abdus8samad@gmail.com>
2020-06-30 06:13:13 +0530

    Added comments to server file

--- server.js
+++ server.js
@@ -2,11 +2,15 @@ const app = require('express')(),
 morgan = require('morgan'),
 PORT = process.env.PORT || 8080;
 
+//Request Logger
 app.use(morgan('dev'));
 
+///This is the home Route
 app.get('/',(req,res) =>{
     res.send('This is the server');
 })
+
+//Starting the Server at a PORT
 app.listen(PORT, () =>{
     console.log(`Listening on PORT ${PORT}`);
 })
